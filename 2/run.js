6aa125a98d7dd8e5d4baf529f7221d4e8849ae5f
thomas_vui <thomas.vuillemin@hinfact.com>
2021-12-15 23:40:06 +0100

    adds day2 part 2

--- 2/run.js
+++ 2/run.js
@@ -3,20 +3,49 @@ import { readFileAndPutLinesInArray } from '../utils/utils.js';
 const pwd = process.cwd();
 const input = readFileAndPutLinesInArray(`${process.cwd()}/input`)
 
-let depth = 0;
-let position = 0;
+const part1 = () => {
+    let depth = 0;
+    let position = 0;
+    
+    input.forEach(element => {
+        const command = element.split(' ');
+        const value = parseInt(command[1]);
+        if (command[0] === 'forward') {
+            position+=value;
+        } else if (command[0] === 'down') {
+            depth+=value;
+        }else if (command[0] === 'up') {
+            depth-=value;
+        }
+    });
+    console.log('part1')
+    console.log('depth: ', depth);
+    console.log('position: ', position);
+    console.log('multiplied: ', depth*position);
+}
 
-input.forEach(element => {
-    const command = element.split(' ');
-    const value = parseInt(command[1]);
-    if (command[0] === 'forward') {
-        position+=value;
-    } else if (command[0] === 'down') {
-        depth+=value;
-    }else if (command[0] === 'up') {
-        depth-=value;
-    }
-});
-console.log('depth: ', depth);
-console.log('position: ', position);
-console.log('multiplied: ', depth*position);
+const part2 = () => {
+    let depth = 0;
+    let position = 0;
+    let aim = 0;
+    
+    input.forEach(element => {
+        const command = element.split(' ');
+        const value = parseInt(command[1]);
+        if (command[0] === 'forward') {
+            position+=value;
+            depth+=aim*value;
+        } else if (command[0] === 'down') {
+            aim+=value;
+        }else if (command[0] === 'up') {
+            aim-=value;
+        }
+    });
+    console.log('part2')
+    console.log('depth: ', depth);
+    console.log('position: ', position);
+    console.log('multiplied: ', depth*position);
+}
+
+part1();
+part2();
